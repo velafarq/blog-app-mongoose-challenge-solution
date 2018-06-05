ffc1d4d71641f158a52f05cfb85c7f5197610e75
velafarq <vela.mrdjen@gmail.com>
2018-06-05 17:30:07 -0500

    Add function for placeholder data

--- tests/test-posts-integration.js
+++ tests/test-posts-integration.js
@@ -11,3 +11,26 @@ const {runServer, app, closeServer} = require('../server');
 const {TEST_DATABASE_URL} = require('../config');
 
 chai.use(chaiHttp);
+
+function seedBlogPostData() {
+    console.info('seeding blog post data');
+    const seedData = [];
+
+    for(let i = 1; i <= 10; i++) {
+        seedData.push(generateBlogPostData());
+    }
+    return BlogPost.insertMany(seedData);
+
+}
+
+function generateBlogPostData() {
+    return {
+        author: {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName()
+          },
+          title: faker.lorem.word(),
+          content: faker.lorem.paragraph(),
+          created: faker.date.past()
+    }
+}
